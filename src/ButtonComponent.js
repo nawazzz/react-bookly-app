d21f0989155be4090346eb0f73fdaef7eb2c2475
nawazzz <65383832+nawazzz@users.noreply.github.com>
2022-04-23 10:37:47 +0200

    restyle button component

--- src/ButtonComponent.js
+++ src/ButtonComponent.js
@@ -1,10 +1,12 @@
 import React from "react";
+import './ButtonComponent.scss'
 
 class ButtonComponent extends React.Component {
     render() {
         return(
-            <div>
+            <div className="buttonContainer">
                 <button
+
                     onClick={(event) => this.props.appendBookInList(event)}
                 >Add</button>
             </div>
