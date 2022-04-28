8005f65d4faa56a7067c586fde297b6021264c49
unknown <khan.nawaz790@gmail.com>
2022-04-28 10:56:54 +0200

    render filteredArray

--- src/List.js
+++ src/List.js
@@ -9,33 +9,59 @@ class List extends React.Component {
         
         return(
             <div>
-                {this.props.allBooks.map((elm, index) => {
-                    return(
-                        <div className='listMainContainer'>
-                            <ul className='bookTitleContainer'>
-                                <span onClick={() => this.props.markAsRead(elm)} >
-                                    <input type={"checkbox"}/>
-                                </span>
-                                <li
-                                    style={{textDecoration: elm.isRead? "underline": "none"}}
-                                >
-                                    {elm.bookName}
-                                </li>
+                { this.props.fileterdBooks.length > 0 ? 
+                    this.props.fileterdBooks.map((elm, index) => {
+                        return(
+                            <div className='listMainContainer'>
+                                <ul className='bookTitleContainer'>
+                                    <span onClick={() => this.props.markAsRead(elm)} >
+                                        <input type={"checkbox"} />
+                                    </span>
+                                    <li
+                                        style={{ textDecoration: elm.isRead ? "underline" : "none" }}
+                                    >
+                                        {elm.bookName}
+                                    </li>
                                     {/* <span onClick={() => {this.props.editList(elm)}}>Edit</span>
                                     <span onClick={(elm) => {this.props.deleteList(elm)}}>x</span> */}
-                                
-                                
-                                {/* <span style={{width: "10px"}} onClick={(event) => {this.props.editList(event)}} ><img style={{width: "100%"}} src={editIcon} alt="edit list"/></span> */}
-                            </ul>
-                            <div className='iconsContainer'>
-                                <span onClick={() => this.props.editList(elm)} ><img src={editIcon} alt="edit list"/></span>
-                                <span onClick={() => this.props.deleteList(elm)}><img src={deleteIcon} alt="delete list"/></span>
-                            </div>  
-                                                      
-                        </div>
-                    )
+
+
+                                    {/* <span style={{width: "10px"}} onClick={(event) => {this.props.editList(event)}} ><img style={{width: "100%"}} src={editIcon} alt="edit list"/></span> */}
+                                </ul>
+                                <div className='iconsContainer'>
+                                    <span onClick={() => this.props.editList(elm)} ><img src={editIcon} alt="edit list" /></span>
+                                    <span onClick={() => this.props.deleteList(elm)}><img src={deleteIcon} alt="delete list" /></span>
+                                </div>
+
+                            </div>
+                        )
+                    }) : this.props.allBooks.map((elm, index) => {
+                            return(
+                                <div className='listMainContainer'>
+                                    <ul className='bookTitleContainer'>
+                                        <span onClick={() => this.props.markAsRead(elm)} >
+                                            <input type={"checkbox"}/>
+                                        </span>
+                                        <li
+                                            style={{textDecoration: elm.isRead? "underline": "none"}}
+                                        >
+                                            {elm.bookName}
+                                        </li>
+                                            {/* <span onClick={() => {this.props.editList(elm)}}>Edit</span>
+                                            <span onClick={(elm) => {this.props.deleteList(elm)}}>x</span> */}
+                                        
+                                        
+                                        {/* <span style={{width: "10px"}} onClick={(event) => {this.props.editList(event)}} ><img style={{width: "100%"}} src={editIcon} alt="edit list"/></span> */}
+                                    </ul>
+                                    <div className='iconsContainer'>
+                                        <span onClick={() => this.props.editList(elm)} ><img src={editIcon} alt="edit list"/></span>
+                                        <span onClick={() => this.props.deleteList(elm)}><img src={deleteIcon} alt="delete list"/></span>
+                                    </div>  
+                                                            
+                                </div>
+                            )
                     
-                })
+                        })
                 }
             </div>
         )
